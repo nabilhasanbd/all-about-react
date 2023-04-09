f1b10daa03f87bd2f6762eeb358037aafe8723b0
Nabil <nabil.uiu.bd@gmail.com>
2023-04-10 03:26:51 +0600

    Added files

--- todo-app-with-dom/script-splits-explanation.js
+++ todo-app-with-dom/script-splits-explanation.js
@@ -5,3 +5,5 @@ item.style.color = "blue";
 for (let element of item) {
   element.style.color = "red";
 }
+
+// Jokhn Ekta block use korbi means oi func er baire oi variable r use hobe na tokhn const korbi r always let korbi and fuck var
