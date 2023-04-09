b823e444ea2973dfe4a0ea4cef8621ee4a5a6be6
Nabil <nabil.uiu.bd@gmail.com>
2023-04-10 01:26:05 +0600

    Added script file

---
+++ todo-app-with-dom/script-splits-explanation.js
@@ -0,0 +1,7 @@
+// my practice
+let item = document.querySelector(".item:nth-child(2");
+item.style.color = "blue";
+
+for (let element of item) {
+  element.style.color = "red";
+}
--- todo-app-with-dom/script.js
+++ todo-app-with-dom/script.js
@@ -68,11 +68,3 @@ for (let i = 0; i < completeUl.children.length; i++) {
 }
 
 form.addEventListener("submit", addTask);
-
-// my practice
-// let item = document.querySelector(".item:nth-child(2");
-// item.style.color = "blue";
-
-// for (let element of item) {
-//   element.style.color = "red";
-// }
