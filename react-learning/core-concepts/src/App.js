ca1dd7d494ca1db3cc48659da1cc5423520d7bab
Nabil <nabil.uiu.bd@gmail.com>
2023-04-29 13:20:18 +0600

    Finish video 3rd

--- react-learning/core-concepts/src/App.js
+++ react-learning/core-concepts/src/App.js
@@ -2,10 +2,27 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  var world = "World";
+  var info = {
+    name: "nabil",
+    age: 26,
+  };
+  var style = {
+    color: "aqua",
+    backgroundColor: "grey",
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p className="" style={style}>
+          Hello {world}
+        </p>
+        <p className="" style={{ backgroundColor: "cyan" }}>
+          I am {info.name} and my age is {info.age}
+        </p>
+        <h4>This is {2000 + 3}</h4>
       </header>
     </div>
   );
