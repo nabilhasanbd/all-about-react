074b88ba422befd493155145bb6af6edf5554b6c
Nabil <nabil.uiu.bd@gmail.com>
2023-05-24 00:50:53 +0600

    Added new button

--- react-learning/my-app/src/components/Textform.js
+++ react-learning/my-app/src/components/Textform.js
@@ -10,6 +10,10 @@ export default function Textform(props) {
     setText(event.target.value);
   };
 
+  const resetText = () => {
+    setText("");
+  };
+
   // text is a string value and setTest is a function
   const [text, setText] = useState("Enter text here");
 
@@ -17,11 +21,22 @@ export default function Textform(props) {
     <div>
       <h2>{props.heading}</h2>
       <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChnage} id="my-box" rows="6"></textarea>
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChnage}
+          id="my-box"
+          rows="6"
+        ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={changeUpCase}>
-        Save
-      </button>
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <button className="btn btn-primary mr-2" onClick={changeUpCase}>
+          Change to uppercase
+        </button>
+        <button className="btn btn-primary" onClick={resetText}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
