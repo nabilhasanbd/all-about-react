3a730819831ba2a9e0bb6e6fc2dcaa6ebca22778
Nabil <nabil.uiu.bd@gmail.com>
2023-05-23 21:09:01 +0600

    Added hook and state

--- react-learning/my-app/src/components/Textform.js
+++ react-learning/my-app/src/components/Textform.js
@@ -1,14 +1,34 @@
 // shortcut rfc
-import React from "react";
+import React, { useState } from "react";
 
 export default function Textform(props) {
+  const changeUpCase = () => {
+    console.log("Upper case was clicked");
+    setText("You have clicked on the changeUpCase button");
+  };
+
+  const handleOnChnage = () => {
+    console.log("text area");
+  };
+
+  // text is a string value and setTest is a function
+  const [text, setText] = useState("Enter text here");
+
   return (
     <div>
       <h2>{props.heading}</h2>
       <div className="mb-3">
-        <textarea className="form-control" id="my-box" rows="6"></textarea>
+        <textarea
+          className="form-control"
+          value={text}
+          onClick={handleOnChnage}
+          id="my-box"
+          rows="6"
+        ></textarea>
       </div>
-      <button className="btn btn-primary">Save</button>
+      <button className="btn btn-primary" onClick={changeUpCase}>
+        Save
+      </button>
     </div>
   );
 }
