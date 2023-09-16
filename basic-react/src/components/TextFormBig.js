bbea1f3b816b94b5cdbb849c997a3b7095056a61
nabil <nabil.uiu.bd@gmail.com>
2023-09-16 18:02:17 +0600

    Added clear button

--- basic-react/src/components/TextFormBig.js
+++ basic-react/src/components/TextFormBig.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 
-
 export default function TextFormBig(prop) {
+    const textToUpper = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const textOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const clearText = () => {
+        // Clear the text
+        setText("");
+    }
+
     const [text, setText] = useState("enter text");
+
     return (
         <div className='container'>
-            <h3>{prop.title} - {text}</h3>
+            <h3>{prop.title}</h3>
             <div className="mb-3">
-            <textarea className="form-control" value={text} id="myTextBox" rows="6"></textarea>
+                <textarea className="form-control" value={text} onChange={textOnChange} id="myTextBox" rows="6"></textarea>
             </div>
-            <button className="btn btn-primary">Chnage to uppercase</button>
+            <button className="btn btn-primary" onClick={textToUpper}>Change to uppercase</button>
+            <button className="btn btn-danger ml-2" onClick={clearText}>Clear Text</button>
         </div>
     )
 }
-
