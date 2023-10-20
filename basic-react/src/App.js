ac77601677f6e209616e3f2a368a742ab7631fe6
nabil <nabil.uiu.bd@gmail.com>
2023-10-20 20:24:01 +0600

    Format code

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -1,14 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextFormBig from './components/TextFormBig';
 
 function App() {
+  const [mode, setMode] = useState('light') // wheather dark mode is enabled or not
   return (
     <>
-      <Navbar title="TetxUtils" /> <br />
+      <Navbar title="TetxUtils" mode={mode} /> <br />
       <TextFormBig title="Enter text" />
-      <About/>
+      <About />
     </>
   );
 }
