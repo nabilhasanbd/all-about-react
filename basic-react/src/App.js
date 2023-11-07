e2be91237a30402d6892262c0d0dfd6a4d7175d9
nabil <nabil.uiu.bd@gmail.com>
2023-11-07 15:54:12 +0600

    Added alert

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -3,23 +3,38 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextFormBig from './components/TextFormBig';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light') // wheather dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
 
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode('light')
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
 
   return (
     <>
       <Navbar title="TetxUtils" mode={mode} toggleMode={toggleMode} /> <br />
+      <Alert alert={alert}/>
       <TextFormBig title="Enter text" mode={mode} />
       {/* <About /> */}
     </>
